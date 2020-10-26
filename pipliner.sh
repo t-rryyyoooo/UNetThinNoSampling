0074ee222119c1619a548f700bef89629add4736
zerebom <ice.choco.pudding.kokoro@gmail.com>
2020-10-26 07:37:32 +0000

    R

--- pipliner.sh
+++ pipliner.sh
@@ -20,6 +20,10 @@ fi
 
 readonly JSON_FILE="${INPUT_DIRECTORY}/${JSON_NAME}"
 
+# run or not
+readonly RUN_TRAINING=$(cat ${JSON_FILE} | jq -r ".run_training")
+readonly RUN_SEGMENTATION=$(cat ${JSON_FILE} | jq -r ".run_segmentation")
+readonly RUN_CALUCULATION=$(cat ${JSON_FILE} | jq -r ".run_caluculation")
 # Training input
 readonly DATASET_PATH=$(eval echo $(cat ${JSON_FILE} | jq -r ".dataset_path"))
 dataset_path="${DATASET_PATH}/image"
@@ -53,7 +57,7 @@ readonly IMAGE_NAME=$(cat ${JSON_FILE} | jq -r ".image_name")
 readonly SAVE_NAME=$(cat ${JSON_FILE} | jq -r ".save_name")
 
 # Caluculation input
-readonly CSV_SAVEPATH=$(eval echo $(cat ${JSON_FILE} | jq -r ".csv_savepath"))
+readonly CSV_SAVEDIR=$(eval echo $(cat ${JSON_FILE} | jq -r ".csv_savedir"))
 readonly CLASS_LABEL=$(cat ${JSON_FILE} | jq -r ".class_label")
 readonly TRUE_NAME=$(cat ${JSON_FILE} | jq -r ".true_name")
 readonly PREDICT_NAME=$(cat ${JSON_FILE} | jq -r ".predict_name")
@@ -76,79 +80,99 @@ do
  log="${LOG}/${key}"
  experiment_name="${EXPERIMENT_NAME}_${key}"
 
- echo "---------- Training ----------"
- echo "Dataset_path:${dataset_path}"
- echo "MODEL_SAVEPATH:${model_savepath}"
- echo "TRAIN_LIST:${TRAIN_LIST}"
- echo "VAL_LIST:${VAL_LIST}"
- echo "MODULE_NAME:${MODULE_NAME}"
- echo "SYSTEM_NAME:${SYSTEM_NAME}"
- echo "CHECKPOINT_NAME:${CHECKPOINT_NAME}"
- echo "LOG:${log}"
- echo "IN_CHANNEL:${IN_CHANNEL}"
- echo "NUM_CLASS:${NUM_CLASS}"
- echo "LEARNING_RATE:${LEARNING_RATE}"
- echo "BATCH_SIZE:${BATCH_SIZE}"
- echo "NUM_WORKERS:${NUM_WORKERS}"
- echo "DROPOUT:${DROPOUT}"
- echo "EPOCH:${EPOCH}"
- echo "GPU_IDS:${GPU_IDS}"
- echo "API_KEY:${API_KEY}"
- echo "PROJECT_NAME:${PROJECT_NAME}"
- echo "EXPERIMENT_NAME:${experiment_name}"
+ run_training_fold=$(echo $RUN_TRAINING | jq -r ".$key")
+ run_segmentation_fold=$(echo $RUN_SEGMENTATION | jq -r ".$key")
+ run_caluculation_fold=$(echo $RUN_CALUCULATION | jq -r ".$key")
 
- python3 train.py ${dataset_path} ${model_savepath} ${MODULE_NAME} ${SYSTEM_NAME} ${CHECKPOINT_NAME} --train_list ${TRAIN_LIST} --val_list ${VAL_LIST} --log ${log} --in_channel ${IN_CHANNEL} --num_class ${NUM_CLASS} --lr ${LEARNING_RATE} --batch_size ${BATCH_SIZE} --num_workers ${NUM_WORKERS} --epoch ${EPOCH} --gpu_ids ${GPU_IDS} --api_key ${API_KEY} --project_name ${PROJECT_NAME} --experiment_name ${experiment_name} --dropout ${DROPOUT}
-
- if [ $? -ne 0 ];then
-  exit 1
- fi
-
- model="${model_savepath}/${MODEL_NAME}"
- echo "---------- Segmentation ----------"
- echo ${test_list[@]}
- for number in ${test_list[@]}
- do
-  image="${DATA_DIRECTORY}/case_${number}/${IMAGE_NAME}"
-  save="${save_directory}/case_${number}/${SAVE_NAME}"
-
-  echo "Image:${image}"
-  echo "Model:${model}"
-  echo "Save:${save}"
-  echo "IMAGE_PATCH_WIDTH:${IMAGE_PATCH_WIDTH}"
-  echo "LABEL_PATCH_WIDTH:${LABEL_PATCH_WIDTH}"
-  echo "OVERLAP:${OVERLAP}"
+ if ${run_training_fold};then
+  echo "---------- Training ----------"
+  echo "Dataset_path:${dataset_path}"
+  echo "MODEL_SAVEPATH:${model_savepath}"
+  echo "TRAIN_LIST:${TRAIN_LIST}"
+  echo "VAL_LIST:${VAL_LIST}"
+  echo "MODULE_NAME:${MODULE_NAME}"
+  echo "SYSTEM_NAME:${SYSTEM_NAME}"
+  echo "CHECKPOINT_NAME:${CHECKPOINT_NAME}"
+  echo "LOG:${log}"
+  echo "IN_CHANNEL:${IN_CHANNEL}"
+  echo "NUM_CLASS:${NUM_CLASS}"
+  echo "LEARNING_RATE:${LEARNING_RATE}"
+  echo "BATCH_SIZE:${BATCH_SIZE}"
+  echo "NUM_WORKERS:${NUM_WORKERS}"
+  echo "DROPOUT:${DROPOUT}"
+  echo "EPOCH:${EPOCH}"
   echo "GPU_IDS:${GPU_IDS}"
+  echo "API_KEY:${API_KEY}"
+  echo "PROJECT_NAME:${PROJECT_NAME}"
+  echo "EXPERIMENT_NAME:${experiment_name}"
 
-  python3 segmentation.py $image $model $save --image_patch_width ${IMAGE_PATCH_WIDTH} --label_patch_width ${LABEL_PATCH_WIDTH} --overlap ${OVERLAP} -g ${GPU_IDS}
+  #python3 train.py ${dataset_path} ${model_savepath} ${MODULE_NAME} ${SYSTEM_NAME} ${CHECKPOINT_NAME} --train_list ${TRAIN_LIST} --val_list ${VAL_LIST} --log ${log} --in_channel ${IN_CHANNEL} --num_class ${NUM_CLASS} --lr ${LEARNING_RATE} --batch_size ${BATCH_SIZE} --num_workers ${NUM_WORKERS} --epoch ${EPOCH} --gpu_ids ${GPU_IDS} --api_key ${API_KEY} --project_name ${PROJECT_NAME} --experiment_name ${experiment_name} --dropout ${DROPOUT}
 
   if [ $? -ne 0 ];then
    exit 1
   fi
 
- done
-
- all_patients="${all_patients}${TEST_LIST} "
-done
-
- echo "---------- Caluculation ----------"
- echo "TRUE_DIRECTORY:${DATA_DIRECTORY}"
- echo "PREDICT_DIRECTORY:${save_directory}"
- echo "CSV_SAVEPATH:${CSV_SAVEPATH}"
- echo "All_patients:${all_patients[@]}"
- echo "NUM_CLASS:${NUM_CLASS}"
- echo "CLASS_LABEL:${CLASS_LABEL}"
- echo "TRUE_NAME:${TRUE_NAME}"
- echo "PREDICT_NAME:${PREDICT_NAME}"
-
-
-python3 caluculateDICE.py ${DATA_DIRECTORY} ${save_directory} ${CSV_SAVEPATH} ${all_patients} --classes ${NUM_CLASS} --class_label ${CLASS_LABEL} --true_name ${TRUE_NAME} --predict_name ${PREDICT_NAME} 
-
- if [ $? -ne 0 ];then
-  exit 1
+ else
+  echo "---------- No training. ----------"
  fi
 
- echo "---------- Logging ----------"
- python3 logger.py ${JSON_FILE}
- echo Done.
+ model="${model_savepath}/${MODEL_NAME}"
+ model_name=${model%.*}
+ csv_name=${model_name////_}
+ if ${run_segmentation_fold};then 
+  echo "---------- Segmentation ----------"
+  echo ${test_list[@]}
+  for number in ${test_list[@]}
+  do
+   image="${DATA_DIRECTORY}/case_${number}/${IMAGE_NAME}"
+   save="${save_directory}/case_${number}/${SAVE_NAME}"
+
+   echo "Image:${image}"
+   echo "Model:${model}"
+   echo "Save:${save}"
+   echo "IMAGE_PATCH_WIDTH:${IMAGE_PATCH_WIDTH}"
+   echo "LABEL_PATCH_WIDTH:${LABEL_PATCH_WIDTH}"
+   echo "OVERLAP:${OVERLAP}"
+   echo "GPU_IDS:${GPU_IDS}"
+
+   #python3 segmentation.py $image $model $save --image_patch_width ${IMAGE_PATCH_WIDTH} --label_patch_width ${LABEL_PATCH_WIDTH} --overlap ${OVERLAP} -g ${GPU_IDS}
+
+   if [ $? -ne 0 ];then
+    exit 1
+   fi
+
+  done
+
+ else
+  echo "---------- No segmentation. ----------"
+ fi
+
+ if ${run_caluculation_fold};then
+  all_patients="${all_patients}${TEST_LIST} "
+ fi
+done
+
+
+echo "---------- Caluculation ----------"
+csv_savepath="${CSV_SAVEDIR}/${csv_name}.csv"
+echo "TRUE_DIRECTORY:${DATA_DIRECTORY}"
+echo "PREDICT_DIRECTORY:${save_directory}"
+echo "CSV_SAVEPATH:${csv_savepath}"
+echo "All_patients:${all_patients[@]}"
+echo "NUM_CLASS:${NUM_CLASS}"
+echo "CLASS_LABEL:${CLASS_LABEL}"
+echo "TRUE_NAME:${TRUE_NAME}"
+echo "PREDICT_NAME:${PREDICT_NAME}"
+
+
+#python3 caluculateDICE.py ${DATA_DIRECTORY} ${save_directory} ${csv_savepath} ${all_patients} --classes ${NUM_CLASS} --class_label ${CLASS_LABEL} --true_name ${TRUE_NAME} --predict_name ${PREDICT_NAME} 
+
+if [ $? -ne 0 ];then
+ exit 1
+fi
+
+echo "---------- Logging ----------"
+#python3 logger.py ${JSON_FILE}
+echo Done.
 
 
